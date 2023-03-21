2300fd99e7165567cead32e3d7ea72536c6ae488
master <pnayimbila@st.ug.edu.gh>
2023-03-21 11:23:04 +0000

    Added an __str__ method

--- BankAccount.py
+++ BankAccount.py
@@ -5,4 +5,8 @@ class BankAccount:
         self.owner = owner
         self.balance = balance
 
+    #Creates an __str__ which makes it possible to pass the object created from the class to the print function
+    def __str__(self):
+        return "This is a Bank Account class"
+
     
