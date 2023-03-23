95a64491d291a360cf6f2ccbeb2a403ef0ebfc0e
master <pnayimbila@st.ug.edu.gh>
2023-03-23 13:27:55 +0000

    Added lines of code that implements the methods in the CheckingAccount class with the various attributes

--- Main.py
+++ Main.py
@@ -2,7 +2,21 @@ from CheckingAccount import CheckingAccount
 from SavingsAccount import SavingsAccount
 
 def main():
+    checkingAccount = CheckingAccount()
 
+    print(checkingAccount)
+
+    checkingAccount.owner = "Percy"
+    print(checkingAccount.owner)
+
+    checkingAccount.balance = 3000
+    print(f"Balance: ${checkingAccount.balance}")
+
+    checkingAccount.withdraw(100)
+    print(f"Current Balance: ${checkingAccount.balance}")
+
+    checkingAccount.deposit(50)
+    print(f"Current Balance: ${checkingAccount.balance}")
 
 if __name__ == "__main__":
     main()
