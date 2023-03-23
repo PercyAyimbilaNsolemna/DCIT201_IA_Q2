cf23dd0a8dd7b31de3c0e8070de9f2669ab435b1
master <pnayimbila@st.ug.edu.gh>
2023-03-23 12:35:44 +0000

    Added an __str__ method to make it possible to pass the object created from the class to the print function

--- SavingsAccount.py
+++ SavingsAccount.py
@@ -5,4 +5,7 @@ class SavingsAccount(BankAccount):
         super()
 
     #Creates an __str__ method that makes it possible for the object created to be passed to the print function
+    def __str__(self):
+        return "This is Savings Account class"
+
     
