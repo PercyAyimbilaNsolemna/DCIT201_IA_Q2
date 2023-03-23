b948a05940e8dd5a752858175ad515dbab7ea14b
master <pnayimbila@st.ug.edu.gh>
2023-03-23 12:33:43 +0000

    Created SavingsAccount class and impoorted the BankAccount class to this class for inheritance

---
+++ SavingsAccount.py
@@ -0,0 +1,8 @@
+from BankAccount import BankAccount
+
+class SavingsAccount(BankAccount):
+    def __init__(self):
+        super()
+
+    #Creates an __str__ method that makes it possible for the object created to be passed to the print function
+    
