5303ffd2e2fd5e60b76a4702a72968ba68b1ec11
master <pnayimbila@st.ug.edu.gh>
2023-03-23 12:40:41 +0000

    Created an implementation for the withdraw abstract method in the BankAccount class

--- SavingsAccount.py
+++ SavingsAccount.py
@@ -1,11 +1,21 @@
 from BankAccount import BankAccount
 
 class SavingsAccount(BankAccount):
-    def __init__(self):
+    def __init__(self, difference=None):
+        self.difference = difference
         super()
 
     #Creates an __str__ method that makes it possible for the object created to be passed to the print function
     def __str__(self):
         return "This is Savings Account class"
 
+    #Creates an implementation for the withdraw abstract method in the BankAccount class
+    def withdraw(self, amount):
+        self.amount = amount 
+        self.difference = self.balance - self.amount
+        if self.difference >= 0:
+            self.balance = self.difference
+        else:
+            raise ValueError("Dear customer you have insufficient balance to complete this transacion")
+    
     
